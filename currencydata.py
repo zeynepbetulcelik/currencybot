5b5ab7f4e9d7b234f21c4c4863f18711efbd72a4
Zeynep Betul Celik <zeynebbettul@gmail.com>
2021-05-09 22:38:41 +0300

    parser

--- currencydata.py
+++ currencydata.py
@@ -1,5 +1,19 @@
 import requests
 import json
+import xml.dom.minidom
+import os 
+
+def parser():
+     for root,dirs,files in os.walk('dir'):
+        for file in files:
+            filename,extension=os.path.splitext(file)
+            if extension=='.json':
+                print(filename)
+
+
+
+
+
 api_key = "<ENTER-KEY-HERE>"
 example_text = "Hollo, wrld" # the text to be spell-checked
 endpoint = "https://finans.truncgil.com/today.json"
@@ -16,4 +30,16 @@ response = requests.post(endpoint, headers=headers, params=params, data=data)
 json_response = response.json()
 for key,value in json_response.items():
     if key=="USD":
-        print(key,value)
+        dolar = value
+        
+val = value
+result = str(val)
+print (value)
+
+
+def main():
+    parser()
+   
+
+if __name__=="__main__":
+    main()
